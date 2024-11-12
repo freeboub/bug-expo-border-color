3e5b5a060957e5a1ff75c3d6b7280b2ddc0b80a5
Olivier Bouillet <olivier@azzapp.com>
2024-11-12 14:41:58 +0100

    chore: update to desmonstrate issue

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,10 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { Image } from 'expo-image';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -22,16 +21,13 @@ export default function RootLayout() {
     }
   }, [loaded]);
 
+  const uri = "https://pngimg.com/uploads/wikipedia/wikipedia_PNG10.png"
+
   if (!loaded) {
     return null;
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-    </ThemeProvider>
+    <Image style={{height: 100, width: 100, borderColor: 'red', borderWidth: 2}} source={{ uri }}/>
   );
 }
